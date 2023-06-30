fe1cad6ea891b21473408775c6750ae65577d6f2
Rajnish <krajnish114@gmail.com>
2023-07-01 00:13:32 +0530

    Location page  ui done

---
+++ JS/headerScroll.js
@@ -0,0 +1,69 @@
+
+const nav = document.querySelector("header");
+
+// Creating variales
+let scroll_position = 30;
+let scroll_direction;
+
+// Show Navbar when dom loads
+nav.classList.add("active__menu");
+// Create an scroll event listener
+window.addEventListener("scroll", () => {
+
+
+    scroll_direction = document.body.getBoundingClientRect().top > scroll_position ? "up" : "down";
+    scroll_position = document.body.getBoundingClientRect().top;
+
+    if (scroll_direction === "up" || $(document).scrollTop() == 0) {
+        // If you are scrolling up, then add active menu class
+        nav.classList.add("active__menu");
+        $('header').css('transform', 'translateY(0)');
+        $('.header').css('opacity', 1);
+    }
+    else {
+        // If you Scrolling down, remove the active menu class
+        nav.classList.remove("active__menu");
+
+        if ($(document).scrollTop() < 10) {
+            $('header').css('transform', 'translateY(0)');
+            $('.header').css('opacity', 0.9);
+        } else if ($(document).scrollTop() < 20) {
+            $('header').css('transform', 'translateY(-1px)');
+            $('.header').css('opacity', 0.7);
+        } else if ($(document).scrollTop() < 30) {
+            $('header').css('transform', 'translateY(-5px)');
+            $('.header').css('opacity', 0.5);
+        } else if ($(document).scrollTop() < 40) {
+            $('.header').css('opacity', 0.2);
+            $('header').css('transform', 'translateY(-10px)');
+        } else if ($(document).scrollTop() < 60) {
+            $('.header').css('opacity', 0.1);
+            $('header').css('transform', 'translateY(-12px)');
+        } else if ($(document).scrollTop() < 80) {
+            $('header').css('transform', 'translateY(-80px)');
+        } else {
+            $('header').css('transform', 'translateY(-88px)');
+            $('.header').css('opacity', 0);
+        }
+    }
+});
+
+$('.hamburger').on('click',function(){
+    $('.HamMenuWrapper').css({
+        'transform':'translateX(0)',
+    });
+    $('header, .contentWrappper,.sixtyFortyWrapper,.swimmingPool,.locationBlocks,.embedMap,.finAssis,.sliderCanvas,.tag-line,.textContenWarpper,.divider,.embedMap,.distanceSection,footer').css('filter','blur(8px)');
+    $('.leftBlurred').delay(300).fadeIn();
+    $('body').css('overflowY','hidden');
+});
+
+$('.closeIco').on('click',function(){
+    $('.HamMenuWrapper').css({
+        'transform':'translateX(100%)'
+    });
+    $('header, .contentWrappper,.sixtyFortyWrapper,.swimmingPool,.locationBlocks,.embedMap,.finAssis,.sliderCanvas,.tag-line,.textContenWarpper,.divider,.embedMap,.distanceSection,footer').css('filter','blur(0)');
+    $('.leftBlurred').hide();
+    $('body').css('overflowY','visible');
+    $('.leftBlurred').delay(300).fadeOut();
+});
+
