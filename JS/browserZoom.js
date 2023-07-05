b50a335aa1c5aed4af94291965faa665f3b523bd
Rajnish <krajnish114@gmail.com>
2023-07-05 23:03:08 +0530

    UI fix 1920 1366

--- JS/browserZoom.js
+++ JS/browserZoom.js
@@ -12,18 +12,31 @@ function getSizes() {
           
  let zoomLevel = Math.floor(zoom+1);
 
- if(zoomLevel<100){
+//  if(zoomLevel<100){
+//     $('.tagline-wrapper').css({
+//         width:"max-content",
+//         margin:"0 auto"
+//     });
+//     $('.parentWrapperBoud').css({
+//         width:"1361px"
+//     });
+//  } else if(zoomLevel>95){
+//     $('.parentWrapperBoud').css({
+//         width:"100%"
+//     });
+//     $('.tagline-wrapper').css({
+//         width:"",
+//         marginLeft: "4%"
+//     });
+//  }
+
+
+if(zoomLevel<100){
     $('.tagline-wrapper').css({
         width:"max-content",
         margin:"0 auto"
     });
-    $('.parentWrapperBoud').css({
-        width:"1361px"
-    });
  } else if(zoomLevel>95){
-    $('.parentWrapperBoud').css({
-        width:"100%"
-    });
     $('.tagline-wrapper').css({
         width:"",
         marginLeft: "4%"
